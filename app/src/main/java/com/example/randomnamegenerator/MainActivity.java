c3fb5e98e2612d2ec2d432f330e1b6bc3b349528
Zenab <zenabbw6@gmail.com>
2020-10-15 12:53:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/randomnamegenerator/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.randomnamegenerator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.button_rand);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, RandomName.class);
+                startActivity(i);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/randomnamegenerator/RandomName.java
@@ -0,0 +1,30 @@
+package com.example.randomnamegenerator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ClipData;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.RandomAccess;
+
+public class RandomName extends AppCompatActivity {
+    TextView name;
+
+    List<String> names = Arrays.asList("Gargi", "Priyanka", "Jayant", "Dhruv", "Zenab");
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_random_name);
+
+        name = (TextView) findViewById(R.id.rand_name);
+        Random rand = new Random();
+        String randomName = names.get(rand.nextInt(names.size()));
+        name.setText(randomName);
+    }
+}
